29cd9e4534642593209c7b35bc1acb3ea31a58e2
olgalambova <lambova25@gmail.com>
2024-05-29 21:43:05 +0300

    dz7.1

---
+++ main.py
@@ -0,0 +1,7 @@
+def say_hi(name:str, age:int):
+    print(f"Hi. My name is {name} and I'm {age} years old")
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+say_hi(name, age)
+
